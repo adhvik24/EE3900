49afd5a7272c6b870549a1c6633c5df484d3c836
Adhvik Murarisetty <74809142+adhvik24@users.noreply.github.com>
2021-08-21 10:14:49 +0530

    Create Assignment1.py

---
+++ Assignment_1/Assignment1.py
@@ -0,0 +1,162 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def dir_vec(A,B):
+  return B-A
+
+def norm_vec(A,B):
+  return np.matmul(omat, dir_vec(A,B))
+
+#Generate line points
+def line_gen(A,B):
+  len =10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*(B-A)
+    x_AB[:,i]= temp1.T
+  return x_AB
+
+def tri_vert(a,b,c):
+  p = (a**2 + c**2-b**2 )/(2*a)
+  q = np.sqrt(c**2-p**2)
+#Triangle vertices
+  A = np.array([p,q]) 
+  B = np.array([0,0]) 
+  C = np.array([a,0]) 
+  return  A,B,C
+
+
+def line_dir_pt(m,A, dim):
+  len = 10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,10,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*m
+    x_AB[:,i]= temp1.T
+  return x_AB
+
+#Foot of the Altitude
+def alt_foot(A,B,C):
+  m = B-C
+  n = np.matmul(omat,m) 
+  N=np.vstack((m,n))
+  p = np.zeros(2)
+  p[0] = m@A 
+  p[1] = n@B
+  #Intersection
+  P=np.linalg.inv(N.T)@p
+  return P
+
+#Intersection of two lines
+def line_intersect(n1,A1,n2,A2):
+  N=np.vstack((n1,n2))
+  p = np.zeros(2)
+  p[0] = n1@A1
+  p[1] = n2@A2
+  #Intersection
+  P=np.linalg.inv(N)@p
+#  P=np.linalg.inv(N.T)@p
+  return P
+
+#Radius and centre of the circumcircle
+#of triangle ABC
+def ccircle(A,B,C):
+  p = np.zeros(2)
+  n1 = dir_vec(B,A)
+  p[0] = 0.5*(np.linalg.norm(A)**2-np.linalg.norm(B)**2)
+  n2 = dir_vec(C,B)
+  p[1] = 0.5*(np.linalg.norm(B)**2-np.linalg.norm(C)**2)
+  #Intersection
+  N=np.vstack((n1,n2))
+  O=np.linalg.inv(N)@p
+  r = np.linalg.norm(A -O)
+  return O,r
+
+#Radius and centre of the incircle
+#of triangle ABC
+def icentre(A,B,C,k1,k2):
+  p = np.zeros(2)
+  t = norm_vec(B,C)
+  n1 = t/np.linalg.norm(t)
+  t = norm_vec(C,A)
+  n2 = t/np.linalg.norm(t)
+  t = norm_vec(A,B)
+  n3 = t/np.linalg.norm(t)
+  p[0] = n1@B- k1*n2@C
+  p[1] = n2@C- k2*n3@A
+  N=np.vstack((n1-k1*n2,n2-k2*n3))
+  I=np.matmul(np.linalg.inv(N),p)
+  r = n1@(I-B)
+  #Intersection
+  return I,r
+
+dvec = np.array([-1,1]) 
+#Orthogonal matrix
+omat = np.array([[0,1],[-1,0]]) 
+
+#using termux
+import subprocess
+import  shlex
+#def destroy(self):
+	#self.close_event()
+
+
+A = np.array([-5,12])
+B = np.array([9,-2])
+
+#finding ratio
+#C={kB+A}/{k+1}
+k=np.linalg.norm(A-C)/np.linalg.norm(C-B)
+print("ratio in which C divides AB is")
+print(k)
+
+
+#Generating all lines
+x_AB = line_gen(A,B)
+
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+
+plt.plot(A[0], A[1], 'o')
+plt.text(A[0] * (1 - 0.1), A[1] * (1 + 0.1) , 'A')
+plt.plot(B[0], B[1], 'o')
+plt.text(B[0] * (1 - 0.03), B[1] * (1 + 0.1) , 'B')
+#coodinates of points
+P = np.array([-8,-5])
+Q = np.array([7,10])
+C = np.array([2,5])
+
+#finding ratio
+#C={kB+A}/{k+1}
+k=np.linalg.norm(P-C)/np.linalg.norm(C-Q)
+print("ratio in which C divides PQ is")
+print(k)
+
+
+#Generating all lines
+x_PQ = line_gen(P,Q)
+
+#Plotting all lines
+plt.plot(x_PQ[0,:],x_PQ[1,:],label='$PQ$')
+
+plt.plot(P[0], P[1], 'o')
+plt.text(P[0] * (1 - 0.1), P[1] * (1 + 0.1) , 'P')
+plt.plot(Q[0], Q[1], 'o')
+plt.text(Q[0] * (1 - 0.03), Q[1] * (1 + 0.1) , 'Q')
+plt.plot(C[0], C[1], 'o')
+plt.text(C[0] * (1 - 0.03), C[1] * (1 + 0.1) , 'C')
+
+
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+
+plt.show()
