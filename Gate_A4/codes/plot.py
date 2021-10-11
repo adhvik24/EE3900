02c0d61a7ce813f5921bb8519b1312b4d2720170
Adhvik Murarisetty <74809142+adhvik24@users.noreply.github.com>
2021-10-11 23:38:25 +0530

    Create plot.py

---
+++ Gate_A4/codes/plot.py
@@ -0,0 +1,58 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.fft import fft
+
+
+N = 500
+x = np.linspace(-5,5, N)
+
+def rectFunction(t):
+    y = np.zeros(N)
+    for i in range(N):
+        if t[i] >= -0.5 and t[i] <= 0.5:
+            y[i] = 1
+    return y
+#Computing the rectangular function
+y1 = rectFunction(x/2)
+#Computing the fourier transform
+y_fourier1 = [10*np.mean(y1 * np.exp(-1j * 2 * np.pi * f * 2*x)) for f in x]
+#Comparing it with the theoretical result
+y_theoretical1 = 2 * (np.sin((2 * np.pi *2* x))/((2 * np.pi * 2*x)))
+
+plt.plot(x,y1)
+plt.xlabel('$t$')
+plt.ylabel('$x(t/2)$')
+plt.grid()
+plt.show()
+
+plt.plot(x,np.real(y_fourier1), label='Simulation',color='b')
+plt.plot(x,np.real(y_theoretical1), label='Theoretical',color='r')
+plt.xlim(-5,5)
+plt.xlabel('$f$')
+plt.ylabel('$X(f)$')
+plt.grid()
+plt.legend()
+plt.show()
+
+
+#Computing the rectangular function
+y2 = rectFunction(x-2)
+#Computing the fourier transform
+y_fourier2 = [10*np.mean(y2 * np.exp(-1j * 2 * np.pi * f * x)) for f in x]
+#Comparing it with the theoretical result
+y_theoretical2 = np.exp(-1j * 2* 2 * np.pi * x) * (np.sin((2 * np.pi * x)/2)/((2 * np.pi * x)/2))
+
+plt.plot(x,y2)
+plt.xlabel('$t$')
+plt.ylabel('$x(t-2)$')
+plt.grid()
+plt.show()
+
+plt.plot(x,np.real(y_fourier2), label='Simulation',color='b')
+plt.plot(x,np.real(y_theoretical2), label='Theoretical',color='r')
+plt.xlim(-5,5)
+plt.xlabel('$f$')
+plt.ylabel('$X(f)$')
+plt.grid()
+plt.legend()
+plt.show()
