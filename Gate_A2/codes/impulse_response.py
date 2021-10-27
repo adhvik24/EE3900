11baf19af0f66d8252b24c40129cec8b75af09a5
Adhvik Murarisetty <74809142+adhvik24@users.noreply.github.com>
2021-10-27 10:14:31 +0530

    Update impulse_response.py

--- Gate_A2/codes/impulse_response.py
+++ Gate_A2/codes/impulse_response.py
@@ -1,23 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn
-seaborn.set(style='ticks')
 
-x = np.linspace(0.2,10,100)
-fig, ax = plt.subplots()
-x=np.linspace(1,80)
-x1=np.linspace(-80,80)
-y1=x1*0
-y=1/((np.pi)*x)
-plt.plot(x,y,color='blue',label="impulse response h(t)")
-plt.plot(-x,-y,color='blue')
-#ax.set_aspect('equal')
-ax.axhline(y=0, color='r')
-ax.axvline(x=0, color='r')
-ax.grid(True, which='both')
-seaborn.despine(ax=ax, offset=0) 
-plt.legend(loc = 'upper right')
-plt.show()
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn
